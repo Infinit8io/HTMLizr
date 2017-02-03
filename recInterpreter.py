5555a9ca489139e93dd221672fa8be9ee509fff0
dominiqu.ducommun <dominique.ducommun@he-arc.ch>
2017-02-03 11:02:12 +0100

    Initial commit

---
+++ recInterpreter.py
@@ -0,0 +1,55 @@
+import AST
+from AST import addToClass
+from functools import reduce
+
+
+operations = {
+    '+' : lambda x, y: x+y,
+    '-' : lambda x, y: x-y,
+    '*' : lambda x, y: x*y,
+    '/' : lambda x, y: x/y
+}
+
+vars = {}
+
+@addToClass(AST.ProgramNode)
+def execute(self):
+    for c in self.children:
+        c.execute()
+
+@addToClass(AST.TokenNode)
+def execute(self):
+    if isinstance(self.tok, str):
+        try:
+            return vars[self.tok]
+        except KeyError:
+            print("*** Error : variable %s undefined !" % self.tok)
+    return self.tok
+
+@addToClass(AST.OpNode)
+def execute(self):
+    args = [c.execute() for c in self.children]
+    if len(args) == 1:
+        args.insert(0,0)
+    return reduce(operations[self.op], args)
+
+@addToClass(AST.AssignNode)
+def execute(self):
+    vars[self.children[0].tok] = self.children[1].execute()
+
+@addToClass(AST.PrintNode)
+def execute(self):
+    print(self.children[0].execute())
+
+@addToClass(AST.WhileNode)
+def execute(self):
+    while self.children[0].execute() != 0: # Tant qu'il y a des noeuds
+        self.children[1].execute() # On exécute
+
+if __name__ == '__main__':
+    from parser5 import parse
+    import sys
+    print("Started Program")
+    prog = open(sys.argv[1]).read()
+    ast = parse(prog)
+    ast.execute()
